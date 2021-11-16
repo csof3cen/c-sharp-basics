0cf44d9986a2eea2577f368f5f710f0ce8a64825
S. Moustapha Sy Ndoye <s.moustapha.sy.ndoye@gmail.com>
2021-11-16 13:07:15 +0000

    Ajoutez des fichiers projet.

---
+++ FirtConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Hello
+{
+    public static void Main(string[] args)
+    {
+        // Principaux types de valeurs d'une variable
+        bool isTrue = false;
+        string name = "Moustapha Ndoye";
+        int age = 15;
+        double lenght = 1.63;
+        char initial = 'M';
+        DateTime today = DateTime.Now;
+
+        System.Console.Write("Prénom : " + name + ", Age : " + age + 
+            Environment.NewLine + "Taille : " + lenght + "m, Initial : " + initial + 
+            Environment.NewLine + "Aujourdh'ui : " + today + ", isTrue : " + isTrue);
+
+        // Les instructions Conditionnelles
+
+    }
+}
